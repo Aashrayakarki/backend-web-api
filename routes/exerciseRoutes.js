59dd281d4ac8e75163eac217ba40bdc2389060e0
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-02 19:15:11 +0545

    added the guards in routes

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const exerciseController = require("../controllers/exerciseController");
+const { adminGuard, authGuard } = require('../middleware/authGuard');
 
 // POST request to create exercise
 router.post('/create', exerciseController.createExercise);
 
 // GET request to fetch all exercises
-router.get('/get_all_exercises', exerciseController.getAllExercises);
+router.get('/get_all_exercises', authGuard, exerciseController.getAllExercises);
 
 // GET request to fetch single exercise
-router.get('/get_exercise/:id', exerciseController.getSingleExercise);
+router.get('/get_exercise/:id', authGuard, exerciseController.getSingleExercise);
 
 // PUT request to update exercise
-router.put('/update/:id', exerciseController.updateExercise);
+router.put('/update/:id', adminGuard, exerciseController.updateExercise);
 
 // DELETE request to delete exercise
-router.delete('/delete/:id', exerciseController.deleteExercise);
+router.delete('/delete/:id', adminGuard, exerciseController.deleteExercise);
 
 // Pagination
 router.get('/pagination', exerciseController.paginationExercises);
