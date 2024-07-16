f8c27fcdcfd2b3fe2b1b5062f75e68296043ac92
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-16 21:45:13 +0545

    added resetPasswordOtp and resetPasswordExpires in user model

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema({
 
     phone:{
         type: Number,
+        unique: true,
         required: true,
     },
 
@@ -47,6 +48,16 @@ const userSchema = new mongoose.Schema({
         required: true,
     },
 
+    resetPasswordOTP:{
+        type: Number,
+        default: null
+    },
+
+    resetPasswordExpires:{
+        type: Date,
+        default: null
+    },
+    
     isAdmin: {
         type: Boolean,
         default: false
