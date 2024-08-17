e855cbcb1608ad7481f5673e164965035a17dc18
Aashrayakarki <aashrayakarki7@gmail.com>
2024-08-17 17:41:30 +0545

    deleted add to routine option

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -262,54 +262,6 @@ const searchExercise = async (req, res) => {
     }
 };
 
-const addToMyExercise = async (req, res) => {
-    const { userId, exerciseId } = req.body;
-
-    if (!userId || !exerciseId) {
-        return res.status(400).json({
-            success: false,
-            message: "User ID and Exercise ID are required"
-        });
-    }
-
-    try {
-        // Find or create the user's exercise routine
-        let userExercise = await UserExercise.findOne({ userId });
-
-        if (!userExercise) {
-            userExercise = new UserExercise({ userId, exercises: [] });
-        }
-
-        // Check if the exercise is already in the user's routine
-        const exerciseExists = userExercise.exercises.some(
-            (exercise) => exercise.exerciseId.toString() === exerciseId
-        );
-
-        if (exerciseExists) {
-            return res.status(400).json({
-                success: false,
-                message: "Exercise already added to your routine"
-            });
-        }
-
-        // Add the exercise to the user's routine
-        userExercise.exercises.push({ exerciseId });
-        await userExercise.save();
-
-        res.status(201).json({
-            success: true,
-            message: "Exercise added to your routine successfully",
-            data: userExercise
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: "Internal server error",
-            error: error
-        });
-    }
-};
 
 module.exports = {
     createExercise,
@@ -319,5 +271,4 @@ module.exports = {
     deleteExercise,
     paginationExercises,
     searchExercise,
-    addToMyExercise 
 };
